2662f52e0e4d2e0328a4c0aab3cda2861eb80c89
Amir <amir.hackett@gmail.com>
2022-03-08 16:50:09 -0500

    added editable tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,27 @@ var taskToDoE1 = document.querySelector("#tasks-to-do");
 
 var creatTaskHandler = function(event) {
 
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
     event.preventDefault();
 
-    var listItemE1 = document.createElement("li");
-    listItemE1.className = "task-item";
-    listItemE1.textContent = "This is a new task.";
-    taskToDoE1.appendChild(listItemE1);
+    // creat list item
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    // give it a class name
+    taskInfoEl.className = "task-info";
+    //add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    //add entire list item to list
+    taskToDoE1.appendChild(listItemEl)
 }; 
 
+//-------------------------------This is a call back
 formEl.addEventListener("submit", creatTaskHandler);
